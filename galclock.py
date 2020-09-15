8d8722da1841182d63a9464f767a11d1d6744755
mohammad-vb-135 <mahmohammad560@gmail.com>
2020-09-15 12:39:59 +0430

    Update galclock.py

--- galclock.py
+++ galclock.py
@@ -1,7 +1,29 @@
-def clocktoone(hurs,minute,socund) :
+def unit1 (hurs,minute,socund) :
     hurs = int (hurs)
     minute = int (minute)
     socund = int (socund)
     return ((((hurs*60)+minute)*60)+socund*100) 
+
+
+
+def multyunit (one):
+    one = int (one)
+    mu = dict()
+    mu ["hurs"] = mu // 360000
+    mu ["minute"] = (mu % 360000) // 6000
+    mu ["socund"] = ((mu % 360000) % 6000) // 100
+    return mu 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
